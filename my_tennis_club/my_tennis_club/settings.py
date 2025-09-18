c5e469e0871b4942873260c5c3490f415f1f430c
digitsmyanmar7-hue <digitsmyanmar7@gmail.com>
2025-09-18 13:31:09 +0630

    Sep 18 , 1:31 pm

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4nj&fx#o(2og%pr#+^ion^3c(&^$m(798+amlr&7d#s338yd@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
